3db49739c61f54fe2a2c90de2cee9991de75f44c
Michael Medvedskiy <michaelmedvedskiyprivate@gmail.com>
2018-07-23 20:43:43 +0300

    trying for heroku 2 attempt

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const publicPath = path.join(__dirname, './../public');
 
 const express = require('express');
-const port = 3000;
+const port = process.env.PORT || 3000;
 console.log(__dirname+'/../public');
 
 console.log(publicPath);
